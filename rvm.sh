107fcb0ccd3f8f8985ffea145e024c9b51cd3608
Marlos Irapuan <marlos.irapuan@gmail.com>
2015-03-10 18:21:10 -0400

    rvm.sh

---
+++ rvm.sh
@@ -0,0 +1,10 @@
+### RVM
+echo installing RVM
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash
+source "$HOME/.rvm/scripts/rvm" >> ~/.bash_profile
+rvm requirements
+echo installing Ruby 2.1.3
+rvm install 2.1.3 --default
+
+echo 'all set, rock on!'
